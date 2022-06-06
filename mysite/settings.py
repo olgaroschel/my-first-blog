1b8851da1c18334d6f1247e4e6c01117685c33e7
Olga Roschel <olgaroschel94@gmail.com>
2022-06-06 12:22:07 +0100

    Hide the secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,9 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
 import dj_database_url
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,15 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ed@4l983f1y5*dscm04_wmz2^bqi)_1x*tjg7dze8u-=mas^8a'
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com'],  [
-    '127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com'],  
+[    '127.0.0.1', '.herokuapp.com'],  
+['127.0.0.1:8000', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
